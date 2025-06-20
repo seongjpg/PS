841d6c9d8281578766863d24095f163bd544e01d
seongjpg <dodoboary@gmail.com>
2025-06-20 18:52:01 +0900

    Clear #15552
    미친 핀토스

---
+++ PS/PS/15552.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+namespace _15552
+{
+    class FastAB
+    {
+        static void Main()
+        {
+            StreamReader rd = new StreamReader(Console.OpenStandardInput());
+            StreamWriter wr = new StreamWriter(Console.OpenStandardOutput());
+            int n = int.Parse(rd.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                string[] ab = rd.ReadLine().Split();
+                wr.Write($"{int.Parse(ab[0]) + int.Parse(ab[1])}\n");
+            }
+                
+            wr.Flush();
+            wr.Close();
+            rd.Close();
+
+        }
+    }
+}
