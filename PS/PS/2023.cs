648a0641f626565ec8b18c9eb9fa6907806f8c44
seongjpg <dodoboary@gmail.com>
2025-07-15 18:07:17 +0900

    Clear #2023
    신기한 문제

---
+++ PS/PS/2023.cs
@@ -0,0 +1,44 @@
+﻿using static System.Console;
+
+namespace _2023
+{
+    class Marvelous
+    {
+        static void IsMystery(int x, int len, int n)
+        {
+            if (len == n)
+            {
+                Write($"{x}\n"); return;
+            }
+            x *= 10;
+            for (int i = 1; i < 10; i++)
+            {
+                if (IsPrime(x + i)) IsMystery(x + i, len + 1, n);
+            }
+
+        }
+        static bool IsPrime(int n)
+        {
+            bool rtn = true;
+            int sqrt = Convert.ToInt32(Math.Sqrt(n));
+            //Write($"sqrt : {sqrt}\n");
+            for (int i = 2; i <= sqrt; i++)
+            {
+                if (n % i == 0)
+                {
+                    rtn = false;
+                    break;
+                }
+            }
+            return rtn;
+        }
+        static void Main()
+        {
+            int n = int.Parse(ReadLine());
+            for (int x = 2; x < 10; x++)
+            {
+                if (IsPrime(x)) IsMystery(x, 1, n);
+            }
+        }
+    }
+}
